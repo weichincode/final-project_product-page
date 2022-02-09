aeaa801c34e422ba62b7d066dacd3a1171313e99
firedrake344 <firedrake344.code@gmail.com>
2022-02-09 09:04:31 +0800

    initial upload

---
+++ js/displayProduct.js
@@ -0,0 +1,14 @@
+const gpu = new Product();
+
+// >> name, description, image, price, sold, quantity
+
+gpu.addProduct(
+  "Asus GTX 3090",
+  "This is an Asus GTX 3090",
+  "./imgs/products/asusGTX3090.jpg",
+  1399,
+  "300 sold, 5/5",
+  "5 available"
+);
+
+gpu.displayProduct();
---
+++ js/productController.js
@@ -0,0 +1,76 @@
+class Product {
+  // properties
+  productItems = [];
+
+  // constructor
+  constructor() {}
+
+  // methods
+  // (1) addProduct method
+  addProduct(name, description, image, price, sold, quantity) {
+    const item = {
+      name: name,
+      description: description,
+      image: image,
+      price: price,
+      sold: sold,
+      quantity: quantity,
+    };
+
+    this.productItems.push(item);
+  } // end of addProduct method
+
+  // (2) displayProduct method
+  displayProduct() {
+    console.log(this.productItems);
+
+    let productDetails = "";
+    let index = 0;
+    let moreBtnId = "";
+    this.productItems.forEach((item) => {
+      moreBtnId = "item" + index;
+      console.log(`Btn id for displayProduct is ${moreBtnId}`);
+
+      productDetails += `
+        <!-- item start -->
+        <div class="card col-lg-3 col-md-6 col-12 border">
+          <img
+            src="${item.image}"
+            class="card-img-top"
+            alt="item image"
+          />
+          <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-text">${item.name}</p>
+            <a id="${moreBtnId}" href="#" class="btn btn-primary" data-toggle="modal" data-target="#productModal">More</a>
+          </div>
+        </div>
+        <!-- item end -->
+      `;
+      index++;
+    }); // end of forEach loop
+
+    document.querySelector("#row").innerHTML = productDetails;
+
+    // display item information when button clicked
+    index = 0;
+    this.productItems.forEach((item) => {
+      moreBtnId = "item" + index;
+      console.log(`Btn id for modal ${moreBtnId}`);
+      document.getElementById(moreBtnId).addEventListener("click", function () {
+        displayItemDetail(item);
+      });
+      index++;
+    });
+  } // end of displayProduct method
+} // end of Product class
+
+// displayItemDetail function for modal
+const displayItemDetail = (item) => {
+  document.querySelector("#itemName").innerText = item.name;
+  document.querySelector("#itemDescription").innerText = item.description;
+  document.querySelector("#itemImage").src = item.image;
+  document.querySelector("#itemPrice").innerText = item.price;
+  document.querySelector("#itemSold").innerText = item.sold;
+  document.querySelector("#itemQuantity").innerText = item.quantity;
+};
